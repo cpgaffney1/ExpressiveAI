deff8b63fbe4ba489be975412ecd3d9afe18d486
unknown <cgaffney@stanford.edu>
2018-02-25 19:16:52 -0800

    test

--- src/rnn.py
+++ src/rnn.py
@@ -26,7 +26,7 @@ def custom_loss(y_true, y_pred):
 
 def train(load=False, bidirectional=False):
     n_step_features = 6
-    reg_weight = 0.00
+    reg_weight = 0.001
     if load:
         if not bidirectional:
             model = load_model("rnn_model.h5")
@@ -42,8 +42,8 @@ def train(load=False, bidirectional=False):
                        kernel_initializer=init))
         else:
             model.add(
-                Bidirectional(LSTM(32, return_sequences=False, activation='relu', kernel_regularizer=reg.l2(reg_weight),
-                       kernel_initializer=init)))
+                Bidirectional(LSTM(32, return_sequences=True, activation='relu', kernel_regularizer=reg.l2(reg_weight),
+                       kernel_initializer=init), input_shape=(util.TIMESTEPS * 2 + 1, n_step_features)))
             model.add(
                 Bidirectional(LSTM(32, return_sequences=False, activation='relu', kernel_regularizer=reg.l2(reg_weight),
                                kernel_initializer=init)))
@@ -85,22 +85,22 @@ def train(load=False, bidirectional=False):
 
 def predict(model):
     musList, musNames, recList, recNames = util.loadSongLists(for_predict=True)
-    for s in range(len(musList)):
-        x_test, y_test = util.collectData([musList[s]], [recList[s]])
+    for i in range(len(musList)):
+        x_test, y_test = util.collectData([musList[i]], [recList[i]], bidirectional=True)
         predictions = np.zeros((x_test.shape[0] + util.TIMESTEPS, 3))
-        predictions[:util.TIMESTEPS + 1, :2] = x_test[0, :, 4:]
-        for i in range(len(x_test)):
+        predictions[:util.TIMESTEPS, :2] = x_test[0, :util.TIMESTEPS, 4:]
+        for j in range(len(x_test)):
             # second half must be all zeros
             input = np.zeros((1, x_test.shape[1], x_test.shape[2]))
             input[0] = x_test[i]
-            input[0, :util.TIMESTEPS + 1, 4:] = predictions[i: i + util.TIMESTEPS + 1, :2]
+            input[0, :util.TIMESTEPS + 1, 4:] = predictions[j: j + util.TIMESTEPS + 1, :2]
             prediction = model.predict_on_batch(input)
-            predictions[i + util.TIMESTEPS] = prediction
+            predictions[j + util.TIMESTEPS] = prediction
 
         predictions[util.TIMESTEPS:] = model.predict(x_test)
-        for i in range(len(y_test)):
-            if y_test[i, 2] == 1:
-                predictions[i + util.TIMESTEPS, 0] = predictions[i + util.TIMESTEPS - 1, 0]
+        for j in range(len(y_test)):
+            if y_test[j, 2] == 1:
+                predictions[j + util.TIMESTEPS, 0] = predictions[j + util.TIMESTEPS - 1, 0]
 
         print('Mean loss: {}'.format(np.mean(np.square(y_test - predictions[util.TIMESTEPS:]))))
         # predictions = note.denormalizeTimes(predictions, recList[0][-1]['end'])
@@ -114,25 +114,31 @@ def predict(model):
             last_rec = recList[i][-1]['end']
             last_mus = musList[i][-1]['end']
             length = musList[i][j]['end'] - musList[i][j]['start']
-            # predicted_start = predictions[j][0] * last_rec / 100.0 + musList[i][j]['start'] * last_rec / last_mus
-            musList[i][j]['start'] = note.denormalizeTimeFromOffsetSubtract(predictions[j][0], musList[i][j],
-                                                                            last_mus, last_rec)
-            # predicted_length = predictions[j][1] * last_rec / 100.0 + length * last_rec / last_mus
-            musList[i][j]['end'] = musList[i][j]['start'] + note.denormalizeTimeFromOffsetSubtract(
+            predicted_start = predictions[j][0] * last_rec / 100.0 + musList[i][j]['start'] * last_rec / last_mus
+            musList[i][j]['start'] = predicted_start
+            '''note.denormalizeTimeFromOffsetSubtract(predictions[j][0], musList[i][j]['start'],
+                                                                            last_mus, last_rec)'''
+            predicted_length = predictions[j][1] * last_rec / 100.0 + length * last_rec / last_mus
+            '''musList[i][j]['end'] = musList[i][j]['start'] + note.denormalizeTimeFromOffsetSubtract(
                 predictions[j][1], length,
-                last_mus, last_rec)
+                last_mus, last_rec)'''
+            musList[i][j]['end'] = musList[i][j]['start'] + predicted_length
             if musList[i][j]['end'] - musList[i][j]['start'] <= 0:
                 musList[i][j]['end'] = musList[i][j]['start'] + 100
-        note.writePIDI(musList[i], path + "//predictions" + str(s) + ".txt")
+        note.writePIDI(musList[i], path + "//predictions" + str(i) + ".txt")
 
 
 
 def main():
-    training_phase = True
+    training_phase = False
+    bidirectional = True
     if training_phase:
-        train(load=True)
+        train(load=True, bidirectional=bidirectional)
     else:
-        model = load_model("rnn_model.h5")
+        if not bidirectional:
+            model = load_model("rnn_model.h5")
+        else:
+            model = load_model("birnn_model.h5")
         predict(model)
 
 # This if statement passes if this
